f04aaf6f2afb6eb818f44a35d3552de575b5ddd4
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-04-08 17:15:29 +0900

    feat: solve getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -235,8 +235,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  return () => {
+    const oldCounter = counter;
+    counter += 1;
+    return oldCounter;
+  };
 }
 
 module.exports = {
