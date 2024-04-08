d4c00b16fd9f665de5eb5834decd395662e66505
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-04-08 16:49:23 +0900

    feat: solve retry task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -141,8 +141,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return (...params) => {
+    let lastError;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func.apply(this, params);
+      } catch (error) {
+        lastError = error;
+      }
+    }
+
+    throw lastError;
+  };
 }
 
 /**
